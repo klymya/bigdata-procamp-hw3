b96e18ef2d614706c4efa15567170141fcb48770
Klym Yamkovyi <yamkovou@gmail.com>
2021-09-17 13:03:46 +0300

    fixed topic creation

--- create_topic.sh
+++ create_topic.sh
@@ -2,7 +2,7 @@ KAFKA_FOLDER=/usr/lib/kafka
 
 $KAFKA_FOLDER/bin/kafka-topics.sh \
 	--create \
-	--bootstrap-server localhost:9092 \
+	--zookeeper localhost:2181 \
 	--topic bitstamp.btc.transactions \
 	--partitions 1 \
 	--replication-factor 2 # replication-factor <= brokers number
